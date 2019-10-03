3ed617c976628688349aaab348b19c333b01b10f
Ronan Silva <contato.ronansilva@gmail.com>
2019-10-03 03:05:43 -0300

    create cors

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require('mongoose');
-
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -14,6 +14,7 @@ mongoose.connect('mongodb+srv://omnistack:4everayzer@cluster0-hqoqg.mongodb.net/
 // req.query = acessar query params ( para filtros)
 // req.params = acessar route params ( para edição, delete)
 // req.body = acessar corpo da requisição ( para criação e edição)
+app.use(cors())
 app.use(express.json()); // informa que o body vem json
 app.use(routes);
 
